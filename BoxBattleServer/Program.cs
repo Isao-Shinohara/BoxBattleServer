90ef0754ddbaeaf1fc6fcd519b356ce4b488d83a
Isao-Shinohara <isao.shinohara.7@gmail.com>
2018-12-29 23:30:30 +0900

    Display MagicOnion log

--- BoxBattleServer/Program.cs
+++ BoxBattleServer/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
+using Grpc.Core.Logging;
 using MagicOnion.Server;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -15,9 +16,15 @@ namespace BoxBattleServer
 		public static void Main(string[] args)
 		{
 			//コンソールにログを表示させる
-			GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+			GrpcEnvironment.SetLogger(new CompositeLogger(
+				new ConsoleLogger()
+			));
 
-			var service = MagicOnionEngine.BuildServerServiceDefinition(isReturnExceptionStackTraceInErrorDetail: true);
+			//--- MagicOnion 側のログを gRPC のログに流し込む
+			//--- そのとき名前付き (JSON 形式) でデータをダンプ
+			var logger = new MagicOnionLogToGrpcLoggerWithNamedDataDump();
+			var options = new MagicOnionOptions(true) { MagicOnionLogger = logger };
+			var service = MagicOnionEngine.BuildServerServiceDefinition(options);
 
 			// localhost:12345でListen
 			var server = new global::Grpc.Core.Server {
