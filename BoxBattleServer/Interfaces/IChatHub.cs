d256ff64fcf41d9e24a1e2f4caf4cfaa6fcc4153
Isao-Shinohara <isao.shinohara.7@gmail.com>
2018-12-30 16:20:56 +0900

    Copy Interfaces

---
+++ BoxBattleServer/Interfaces/IChatHub.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using MagicOnion;
+
+namespace BoxBattle.Interfaces
+{
+    public interface IChatHub : IStreamingHub<IChatHub, IChatHubReceiver>
+    {
+        Task JoinAsync(string userName);
+        Task LeaveAsync();
+        Task SendMessageAsync(string message);
+    }
+}
